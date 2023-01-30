30e436c9436a9e8f54ada895d6f666bfe3206d31
Brahim Seriah <dev.sbrahim@gmail.com>
2023-01-30 20:04:57 +0100

    fixing tailwindcss and import custom font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "app-primary": "#8A1538",
+        "app-secondary": "#FF004C",
+      },
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"],
+        Tajawal: ["Tajawal, sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
